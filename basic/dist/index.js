413641ad089d890933ad20ac93a08822309a6d82
goodaseul <gongdue3@gmail.com>
2024-07-30 11:18:57 +0900

    typescript basic

---
+++ basic/dist/index.js
@@ -0,0 +1,6 @@
+let a = 3;
+// console.log(a);
+function addNumber(a, b) {
+    return a + b;
+}
+addNumber(3, 7);
---
+++ basic/index.ts
@@ -0,0 +1,9 @@
+let a: number = 3;
+
+// console.log(a);
+
+function addNumber(a: number, b: number): number {
+    return a + b;
+}
+
+addNumber(3, 7);
